1d9e7bf5cc301fc09b50f20c0c700413b52288d4
Anastasia Treshchina <anastasiya.treschina@gmail.com>
2022-02-17 14:31:29 +0400

    Format

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,7 +4,7 @@ public class SQRService {
     public int howManySquares(int startNumber, int lastNumber) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i <= lastNumber &&  startNumber <= i * i){
+            if (i * i <= lastNumber && startNumber <= i * i) {
                 count++;
             }
         }
